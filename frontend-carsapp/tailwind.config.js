475bf15d4ccdc385686a2b90a9cba5714a21e44e
Sebastian <sebastian.sloniec@gmail.com>
2021-11-18 16:36:15 +0100

    FRONTEND: Initialize project with typescript, redux, tailwind CSS/Craco

---
+++ frontend-carsapp/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: ["./src/**/*.{js, tsx, ts, tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
