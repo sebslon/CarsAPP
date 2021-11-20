95a7459962e9e6bbf6cead1c5338f1395853aaf5
Sebastian <sebastian.sloniec@gmail.com>
2021-11-20 12:44:57 +0100

    BACKEND: Add some validation to user input and enable CORS

--- backend-carsapp/src/main.ts
+++ backend-carsapp/src/main.ts
@@ -1,9 +1,12 @@
-import { BaseExceptionFilter, NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new BaseExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors();
 
   await app.listen(process.env.PORT || 9000);
 }
